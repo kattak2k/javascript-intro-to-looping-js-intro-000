d2c93e019eb2f67a6b5d1756ade15762b63e72a0
Ravi <kkiran.ror@gmail.com>
2020-03-12 00:25:48 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 let arr = []
 
 function forLoop(array){
-   for (let i = 0; i < 25; i++) {
+   for (let i = 0; i <= 25; i++) {
      arr[i] = `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
    }
    console.log(arr)
